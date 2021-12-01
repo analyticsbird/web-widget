22efb6076c170b34460480c84b1998d8c62eec49
Rolwin Monterio <rolwin@yellow.ai>
2021-12-01 19:47:29 +0530

    removed unused imports

--- src/components/DetailModal.tsx
+++ src/components/DetailModal.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useContext, useState } from 'react';
-import { ConfigContext } from '../context/context';
+import React from 'react';
 import FeedbackForm from './FeedbackForm';
-import ThankYou from './ThankYou';
 
 interface PropTypes{
     setCloseModal: (close: boolean) => void;
